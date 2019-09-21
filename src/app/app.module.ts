a6fa663cd411ef4da065454dfcc88b5653b44d00
Pieter-Jan Vaernewijck <pieter-jan.vaernewijck@actemium.com>
2019-09-21 13:52:23 +0200

    scanner button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatListModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatProgressSpinnerModule,
+  MatTooltipModule,
+  MatRippleModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    FlexLayoutModule,
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
